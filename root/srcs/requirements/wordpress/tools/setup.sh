fb10cc0473b7ee4de5bbbfbe56bca7dc258a2e07
rda-cunh <ricard.mendes@gmail.com>
2026-01-21 19:51:28 +0000

    started setup.php for wordpress

--- root/srcs/requirements/wordpress/tools/setup.sh
+++ root/srcs/requirements/wordpress/tools/setup.sh
@@ -1,1 +1,11 @@
-# TO DO
+#!/bin/bash
+
+# change the owner of the wordpress files to www-data user
+chown -R www-data:www-data /var/www/inception/
+
+# move the wp-config.php file to the wordpress folder (if not already there)
+if [ ! -f /var/www/inception/wp-config.php ]; then
+	mv /tmp/wp-config.php /var/www/inception/
+fi
+
+# temporary variables to be replaced later with a proper .env file
