42701d5998519c3c65e77c63d0e2007282e95155
rda-cunh <ricard.mendes@gmail.com>
2026-01-22 02:02:03 +0000

    completed setup script for wordpress

--- root/srcs/requirements/wordpress/tools/setup.sh
+++ root/srcs/requirements/wordpress/tools/setup.sh
@@ -8,4 +8,42 @@ if [ ! -f /var/www/inception/wp-config.php ]; then
 	mv /tmp/wp-config.php /var/www/inception/
 fi
 
-# temporary variables to be replaced later with a proper .env file
+# temporary variables to be replaced later with a proper .env file [TO DELETE]
+WP_URL=login.42.fr
+WP_TITLE=Inception
+WP_ADMIN_USER=theroot
+WP_ADMIN_PASSWORD=123
+WP_ADMIN_EMAIL=theroot@123.com
+WP_USER=theuser
+WP_PASSWORD=abc
+WP_EMAIL=theuser@123.com
+WP_ROLE=editor
+
+# dowload the wordpress files
+sleep 10
+wp --allow-root --path="/var/www/inception/" core download || true
+
+# if files aren't present create install wordpress and set it (if not continue)
+if ! wp --allow-root --path="/var/www/inception/" core is-installed;
+then
+    wp  --allow-root --path="/var/www/inception/" core install \
+        --url=$WP_URL \
+        --title=$WP_TITLE \
+        --admin_user=$WP_ADMIN_USER \
+        --admin_password=$WP_ADMIN_PASSWORD \
+        --admin_email=$WP_ADMIN_EMAIL
+fi;
+
+# create a non admin user
+if ! wp --allow-root --path="/var/www/inception/" user get $WP_USER;
+then
+    wp  --allow-root --path="/var/www/inception/" user create \
+        $WP_USER \
+        $WP_EMAIL \
+        --user_pass=$WP_PASSWORD \
+        --role=$WP_ROLE
+fi;
+
+# start the php server
+exec $@
+
